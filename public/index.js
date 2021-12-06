810f5b4dc33fa2430c0f033012d60410580162da
Jeremy Brown <jlbmagic@gmail.com>
2021-12-06 09:49:54 -0500

    basic set up

--- public/index.js
+++ public/index.js
@@ -6,8 +6,31 @@ var table;
 window.loadData = function (json) {
   var obj = JSON.parse(json); // data from FM is a string
   var data = obj.data;
+  var config = obj.config;
+  var script = config.script
+  console.log(script);
 var columns = obj.columns;
 console.log(columns);
+
+
+const ell = (c) =>{
+  return function(data,type,row){
+    return data.length > c ? data.substr(0,c)+'...' : data;
+  }
+}
+
+const toolTip = (c) =>{
+
+  return function(data,type,row){
+    return '<div class="tooltip">'+data+'</div>';
+  }
+}
+columns.forEach(function(column){
+  // column.elipisis = 3
+  column.render= column.ellipsis ? ell(column.ellipsis) : undefined;
+column.mRender = toolTip()
+});
+
   // create column headers from data
   // var firstRecord = data[0];
   // var columns = Object.keys(firstRecord.fieldData).map(function (key) {
@@ -27,19 +50,34 @@ console.log(columns);
   table = $("#example").DataTable({
     paging: true,
     searching: true,
-    scrollY: "536",
+    scrollY: $(window).height() - 150,
+    height:"100%",
     colReorder: true,
     columns: columns,
     data: data,
+   
   });
 
   // Add the click handler to the row, after removing it if already exists
-  $("#example tbody").off("dblclick");
-  $("#example tbody").on("dblclick", "tr", function () {
+  $("#example tbody").off("click");
+  $("#example tbody").on("click", "tr", function () {
     var record = table.row(this).data();
     var json = JSON.stringify(record);
 
-    FileMaker.PerformScript("On Double Click Row", json);
+    FileMaker.PerformScript(script, json);
   });
+  $('#example tbody').on('mousemove', 'tr', function(e) {
+    var rowData = table.row(this).data()
+    console.log(rowData);
+    // return
+    $("#tooltip").text("Hi").animate({ left: e.pageX, top: e.pageY }, 1)
+    if (!$("#tooltip").is(':visible')) $("#tooltip").show()
+  })
+  $('#example tbody').on('mouseleave', function(e) {
+    $("#tooltip").hide()
+  })  
+  $.fn.dataTable.ext.errMode = "none";
+  $("#example").css("height", $(window).height() - 150);
 };
-$.fn.dataTable.ext.errMode = "none";
+
+
