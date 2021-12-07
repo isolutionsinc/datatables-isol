b51ef5c79e7f3eec5b3bbb211827c9492a858368
Jeremy Brown <jlbmagic@gmail.com>
2021-12-07 16:58:15 -0500

    added image rendering better

--- public/index.js
+++ public/index.js
@@ -10,28 +10,35 @@ const defaultConfig = {
   colReorder: true,
  
 };
-
+let template;
 // exposing loadData to FileMaker Script
 window.loadData = function (json) {
   var obj = JSON.parse(json); // data from FM is a string
+  console.log(obj);
   var data = obj.data;
   var config = obj.config;
+  // const clickType = config.clickType || "cell"
   const globalConfig = config.globals || {};
   var script = config.script
 var columns = obj.columns;
 
-const template = columns
+columns.forEach(elm => {
+elm.columnType ==="img"? 
+  elm.render = function (data, type, row, meta) {
+    return `<img src="${data}" oneerror='this.oneerror=null' alt='' class="img-responsive" />`
+}:null
+  return elm;
+})
+ template = columns
   .map((elm) => elm.data)
   .reduce((acc, curr) => ((acc[curr] = ""), acc), {});
 
-console.log(template)
 
 
 const dataUpdated = data.map((elm) => {
   return { ...template, ...elm };
 });
 
-console.log("data updated", dataUpdated)
 const ell = (c) =>{
   return function(data,type,row){
     return data.length > c ? data.substr(0,c)+'...' : data;
@@ -39,18 +46,14 @@ const ell = (c) =>{
 }
 
 
-const addImgSrc = (c) =>{ 
-    console.log(1)
-    const img = "<img src='"+c+"' onerror='this.onerror=null' alt=''/>";
-    console.log("IMG",img);
-    return img
-  }
+// const addImgSrc = (c) =>{ 
+//     const img = "<img src='"+c+"' onerror='this.onerror=null' alt=''/>";
+//     return img
+//   }
 
 const globals = {...defaultConfig,...globalConfig};
 
 dataUpdated.forEach(function(d){
-
-  d.thumbnail_url = addImgSrc(d.thumbnail_url);
   d.render= d.ellipsis ? ell(d.ellipsis) : undefined;
 });
 globals.columns = columns;
@@ -63,24 +66,20 @@ globals.data = dataUpdated;
   // Add the click handler to the row, after removing it if already exists
  if (script) {
   $("#example tbody").off("click");
-  $("#example tbody").on("click", "tr", function () {
-    var record = table.row(this).data();
-    var json = JSON.stringify(record);
 
-    FileMaker.PerformScript(script, json);
-  });
+  
+  $("#example tbody").on("click", "td", function () {
+  var cell = table.cell(this);
+  const cellObj = cell[0][0];
+const value = cell.data()
+const col = table.column(this).index();
+const row = table.row(this).index();
+console.log("ROW",dataUpdated[row]);
+const json = {value, column:columns[col],row:dataUpdated[row]}
+console.log(json);
+FileMaker.PerformScript(script, JSON.stringify(json));
+
+ });
 }
-  // $('#example tbody').on('mousemove', 'tr', function(e) {
-  //   var rowData = table.row(this).data()
-  //   console.log(rowData);
-  //   // return
-  //   $("#tooltip").text("Hi").animate({ left: e.pageX, top: e.pageY }, 1)
-  //   if (!$("#tooltip").is(':visible')) $("#tooltip").show()
-  // })
-  // $('#example tbody').on('mouseleave', function(e) {
-  //   $("#tooltip").hide()
-  // })  
-  $.fn.dataTable.ext.errMode = "none";
-};
-
-
+ $.fn.dataTable.ext.errMode = "none";
+}
