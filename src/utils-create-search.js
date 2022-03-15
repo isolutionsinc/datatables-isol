84947e103365537cac7326955f8bdf6162348907
Jeremy Brown <jlbmagic@gmail.com>
2022-03-15 14:30:08 -0400

    updated filters to be dropdowns

--- src/utils-create-search.js
+++ src/utils-create-search.js
@@ -4,6 +4,7 @@ export const createDivs = (columns, table) => {
   const row = document.createElement("div");
   row.classList.add("row");
   columns.forEach((column, i) => {
+    const columnType = column.columnType;
     if (!column.searchable) {
       return;
     }
@@ -23,16 +24,35 @@ export const createDivs = (columns, table) => {
     inputSpan.id = "searchLabel";
     inputSpan.innerHTML = column.title;
 
-    const inputField = document.createElement("input");
-    inputField.classList.add("form-control");
+    const inputField = document.createElement("select");
+    inputField.classList.add("custom-select");
     inputField.classList.add("pl-3");
     inputField.id = `${column.data}Search`;
-    inputField.type = "search";
-    inputField.addEventListener("keyup", function () {
-      console.log(this.value);
+    inputField.addEventListener("change", function () {
+      console.log(i, this.value);
       table.columns(i).search(this.value).draw();
     });
-
+    const option = document.createElement("option");
+    option.value = "";
+    option.innerHTML = "Choose . . .";
+    inputField.appendChild(option);
+    const uniqueData = table
+      .column(i)
+      .data()
+      .unique()
+      .filter((e) => e !== "");
+    console.log(uniqueData.length);
+    for (j = 0; j < uniqueData.length; j++) {
+      console.log(uniqueData[j]);
+      const dataPoint =
+        columnType === "dateTime"
+          ? moment(uniqueData[j]).format("MM/D/YYYY")
+          : uniqueData[j];
+      const option = document.createElement("option");
+      option.value = dataPoint;
+      option.innerHTML = dataPoint;
+      inputField.appendChild(option);
+    }
     inputPrepend.appendChild(inputSpan);
     inputGroup.appendChild(inputPrepend);
     inputGroup.appendChild(inputField);
